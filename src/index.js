7d6c1b91b7805c941512c0aceef3aaf7a275b6ce
Anna Barmina <anutik-barmina@mail.ru>
2020-10-05 02:29:37 +0200

    added sidebar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import './styles/main.css'
 
-import {model} from './model';
+import { model } from './model';
+import { App } from './classes/app';
 
-const $site = document.querySelector('#site')
-
-model.forEach(block => {
-    $site.insertAdjacentHTML('beforeend', block.toHTML())
-});
+new App(model).init();
